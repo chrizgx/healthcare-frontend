8f0c1eca50080714b0da3e9d8e89ce2d55c28977
Chris <chrizgeorgoulis@gmail.com>
2024-09-03 01:52:33 +0300

    Insert tab bar component in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TabBarComponent } from './tab-bar/tab-bar.component';
 import { LaunchBarComponent } from './launch-bar/launch-bar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [LaunchBarComponent],
+  imports: [TabBarComponent, LaunchBarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
