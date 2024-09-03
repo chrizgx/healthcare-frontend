a8ff55686b86e0e7dca1eed9e4fa80748fcc42f6
Chris <chrizgeorgoulis@gmail.com>
2024-09-03 21:31:34 +0300

    Implement functionality to add and track component instances

--- src/app/services/window-manager.service.ts
+++ src/app/services/window-manager.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+interface ComponentInstance {
+  id: string;
+  component: any;
+  title: string;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WindowManagerService {
 
-  constructor() { }
+  public instances: ComponentInstance[] = [];
+  private activeInstanceId = new BehaviorSubject<string | null>(null);
+  public get activeInstance$() {
+    return this.activeInstanceId.asObservable();
+  }
+
+  // 1
+  // constructor() { }
+  setActiveInstance(instanceId: string): void {
+    this.activeInstanceId.next(instanceId);
+  }
+
+  //2
+  addInstance(componentType: any, title: string): string {
+    const newId = this.generateInstanceId();
+
+    const newInstance: ComponentInstance = {
+      id: newId,
+      component: componentType,
+      title: title
+    };
+
+    this.instances.push(newInstance);
+    this.setActiveInstance(newId);
+
+    console.log(this.instances);
+
+    return newId;
+  }
+
+  // 3
+  private generateInstanceId(): string {
+    return 'instance-' + Math.random().toString(36).substring(2, 15);
+  }
 }
