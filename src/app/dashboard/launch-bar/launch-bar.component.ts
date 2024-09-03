2f80b1db86b274da750a29883a65c6c4decd16db
Chris <chrizgeorgoulis@gmail.com>
2024-09-03 21:38:45 +0300

    Launchbar now dynamically shows icons and calls component instance generation

--- src/app/dashboard/launch-bar/launch-bar.component.ts
+++ src/app/dashboard/launch-bar/launch-bar.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { inject, Component } from '@angular/core';
+
+// Import components to include in window generation methods
+import { ChatComponent } from '../../components/chat/chat.component';
+import { DepartmentComponent } from '../../components/department/department.component';
+import { PatientComponent } from '../../components/patient/patient.component';
+import { WindowManagerService } from '../../services/window-manager.service';
 
 @Component({
   selector: 'app-launch-bar',
@@ -8,5 +14,37 @@ import { Component } from '@angular/core';
   styleUrl: './launch-bar.component.scss'
 })
 export class LaunchBarComponent {
+  private windowManager: WindowManagerService = inject(WindowManagerService);
+
+  components = [
+    {
+      component: PatientComponent,
+      id: 1,
+      title: 'Patients',
+      icon: 'person',
+      roles: null,
+    },
+    {
+      component: DepartmentComponent,
+      id: 2,
+      title: 'Department',
+      icon: 'building',
+      roles: null,
+    },
+    {
+      component: ChatComponent,
+      id: 3,
+      title: 'Chat',
+      icon: 'chat',
+      roles: null,
+    }
+  ]
+
+  launchComponent(id: number) {
+    const component = this.components.find(c => c.id === id);
+    if (component) {
+      this.windowManager.addInstance(component.component, component.title);
+    }
+  }
 
 }
