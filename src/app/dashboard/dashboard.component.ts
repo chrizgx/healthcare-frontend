6d328bb6a7c8ff14f8116595cc53243d68dd38b8
Chris <chrizgeorgoulis@gmail.com>
2024-09-03 21:46:15 +0300

    Dashboard loads the window component that displays currently active window

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TabBarComponent } from './tab-bar/tab-bar.component';
+import { WindowComponent } from './window/window.component';
 import { LaunchBarComponent } from './launch-bar/launch-bar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [TabBarComponent, LaunchBarComponent],
+  imports: [TabBarComponent, WindowComponent, LaunchBarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
