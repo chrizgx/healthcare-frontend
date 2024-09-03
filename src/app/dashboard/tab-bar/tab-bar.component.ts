c494686791da44fbc001c556c6981f77179fceb8
Chris <chrizgeorgoulis@gmail.com>
2024-09-04 01:43:09 +0300

    Implement tab conditional generation and active tab tracker

--- src/app/dashboard/tab-bar/tab-bar.component.ts
+++ src/app/dashboard/tab-bar/tab-bar.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { inject, Component } from '@angular/core';
+import { WindowManagerService } from '../../services/window-manager.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-tab-bar',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './tab-bar.component.html',
   styleUrl: './tab-bar.component.scss'
 })
 export class TabBarComponent {
+  protected windowManager: WindowManagerService = inject(WindowManagerService);
 
 }
